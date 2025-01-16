01a9585a0f0982bdaadd2da68ea060c4c4a2ce19
田中成来 <tanaka@g-hill.jp>
2025-01-16 23:29:36 +0900

    pcざっくり完了

--- js/main.js
+++ js/main.js
@@ -27,31 +27,85 @@ function initIntersectionObserver() {
 // Swiper
 ///////////////////////////////////////////////////////////////////////////////////////
 function initSwipers() {
-  // Under Press Swiper
-  const underPressElement = document.querySelector(".p-projects_swiper");
-  if (underPressElement) {
-    new Swiper(".p-projects_swiper", {
-      slidesPerView: 1.1,
+  const worksElement = document.querySelector(".swiper-works");
+  if (worksElement) {
+    new Swiper(".swiper-works", {
       loop: true,
       speed: 1000,
       allowTouchMove: false,
-      spaceBetween: 14,
+      slidesPerView: 1.1,
+      spaceBetween: 9,
+      centeredSlides: true, // アクティブなスライドを中央にする
+      // 自動再生
       autoplay: {
         delay: 3000,
         disableOnInteraction: false,
       },
+      // ページネーション
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+      // 前後の矢印
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
       breakpoints: {
         769: {
           slidesPerView: "3.4",
-          loopedSlides: 5,
+        },
+      },
+    });
+  }
+
+  const voiceElement = document.querySelector(".swiper-voice");
+  if (voiceElement) {
+    new Swiper(".swiper-voice", {
+      loop: true,
+      speed: 1000,
+      allowTouchMove: false,
+      slidesPerView: 1.1,
+      spaceBetween: 82,
+      centeredSlides: true, // アクティブなスライドを中央にする
+      // 自動再生
+      autoplay: {
+        delay: 5000,
+        disableOnInteraction: false,
+      },
+      // ページネーション
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+      // 前後の矢印
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+      breakpoints: {
+        769: {
+          slidesPerView: "1.4",
         },
       },
     });
   }
 }
-
 // DOM Ready時の初期化
 document.addEventListener("DOMContentLoaded", function () {
   initIntersectionObserver();
   initSwipers();
+});
+
+///////////////////////////////////////////
+//FAQ アコーディオン
+///////////////////////////////////////////
+$(".qa-list dd").hide();
+$(".qa-list dl").on("click", function (e) {
+  $("dd", this).slideToggle("fast");
+  if ($(this).hasClass("open")) {
+    $(this).removeClass("open");
+  } else {
+    $(this).addClass("open");
+  }
 });
